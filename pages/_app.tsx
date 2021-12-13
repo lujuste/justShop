5c7b39d7527ea63d00df60fd91bb78287f83a359
Lucas Juste <lu.scatollini@hotmail.com>
2021-12-12 21:27:28 -0300

    Add list products

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
