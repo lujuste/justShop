4fec7f5dacfd5b99b09f9c44975f4d5d7a7d38c4
Lucas Juste <lu.scatollini@hotmail.com>
2021-12-18 23:49:10 -0300

    List Products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,15 @@
 import type { NextPage } from 'next'
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
+import data from '../utils/data'
 
 import Layout from '../components/Layout'
 
@@ -7,11 +18,31 @@ const Home: NextPage = () => {
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 1</li>
-          <li>Product 1</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map(product => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography> ${product.price} </Typography>
+                  <Button size="small" color="primary">
+                    {' '}
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   )
